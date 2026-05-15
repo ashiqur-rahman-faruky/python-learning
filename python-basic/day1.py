bb84d46ea1dbc9e988ad2bde5d473849bd6ca855
ashiqur-rahman-faruky <mdrahmanashiur3@gmail.com>
2026-05-15 12:03:39 +0600

    day wise classes

---
+++ python-basic/day1.py
@@ -0,0 +1,13 @@
+# variables
+name = 'Ashiqur Rahman';
+age = 28;
+
+# print('My name is', name);
+print(f'Hi {name}, you are {age} years old');
+
+# input
+# name = input('What is your name? ');
+# age = input('What is your age? ');
+
+# print('Hello', name);
+# print('You are', age, 'years old');
---
+++ python-basic/day2.py
@@ -0,0 +1,12 @@
+age = int(input('What is your age? '));
+
+if(age < 0):
+    print('Invalid age');
+elif(age <= 12):
+    print('You are a child');
+elif(age <= 17):
+    print('You are a teenager');
+elif(age <= 59):
+    print('You are an adult');
+else:    
+    print('You are a senior citizen');
